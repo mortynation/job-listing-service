b5b3632e54f0b24d09cedfefdf81d8761ef1e40f
mortynation <nivaasanna24@gmail.com>
2023-07-16 10:40:48 -0400

    trying to add swagger to the service, apart from swagger dep added javax servlet dep and mvc pathmatch property to yml to solve compatibility issues

--- src/main/java/com/joblisting/joblistingservice/JobListingServiceApplication.java
+++ src/main/java/com/joblisting/joblistingservice/JobListingServiceApplication.java
@@ -1,10 +1,10 @@
 package com.joblisting.joblistingservice;
 
-import com.google.common.base.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -13,8 +13,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class JobListingServiceApplication {
 
     public static void main(String[] args) {
