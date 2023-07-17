0a7f105617b803a810899d0333cb574ac3ea0f25
mortynation <nivaasanna24@gmail.com>
2023-07-17 12:09:29 -0400

    added swagger UI with project info

--- src/main/java/com/joblisting/joblistingservice/JobListingServiceApplication.java
+++ src/main/java/com/joblisting/joblistingservice/JobListingServiceApplication.java
@@ -1,23 +1,31 @@
 package com.joblisting.joblistingservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Job Listing Service",
+                version = "1.0.0",
+                description = "Enable's users to add and retrieve job postings",
+                termsOfService = "nope",
+                contact = @Contact(
+                        name = "Nivaas",
+                        email = "nivaasanna24@gmail.com"
+                    ),
+                license = @License (name = "license", url = "nope")
+        )
+)
 public class JobListingServiceApplication {
 
     public static void main(String[] args) {
